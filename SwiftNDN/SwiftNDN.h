34fae568b1369d8298621f5576bcb22e15956ad7
Wentao Shang <wentaoshang@gmail.com>
2015-02-27 17:46:21 -0800

    Initial Commit

---
+++ SwiftNDN/SwiftNDN.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNDN.h
+//  SwiftNDN
+//
+//  Created by Wentao Shang on 2/27/15.
+//  Copyright (c) 2015 Wentao Shang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftNDN.
+FOUNDATION_EXPORT double SwiftNDNVersionNumber;
+
+//! Project version string for SwiftNDN.
+FOUNDATION_EXPORT const unsigned char SwiftNDNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNDN/PublicHeader.h>
+
+
