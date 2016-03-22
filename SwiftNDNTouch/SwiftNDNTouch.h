e5315579f75f950730af67a1b18989783fe3ef48
Wentao Shang <wentaoshang@gmail.com>
2016-03-22 15:44:53 -0700

    update source code organization

---
+++ SwiftNDNTouch/SwiftNDNTouch.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNDNTouch.h
+//  SwiftNDNTouch
+//
+//  Created by Wentao Shang on 3/22/16.
+//  Copyright © 2016 Wentao Shang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftNDNTouch.
+FOUNDATION_EXPORT double SwiftNDNTouchVersionNumber;
+
+//! Project version string for SwiftNDNTouch.
+FOUNDATION_EXPORT const unsigned char SwiftNDNTouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNDNTouch/PublicHeader.h>
+
+#import "GCDAsyncSocket.h"
